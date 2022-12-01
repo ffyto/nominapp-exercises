59304a075f241734e55f47cfc2ad9303bc6c2ae6
Mauro Arango <cmauricio.arango@gmail.com>
2022-11-30 21:02:47 -0500

    first ruby project

---
+++ class_Reto_5.rb
@@ -0,0 +1,61 @@
+class Reto_5
+
+  attr_writer :file_name
+
+  def initialize(file_name)
+    @file_name = file_name
+  end
+
+  def read_file(file_name)
+    questions_list = []
+    question = {}
+    answer = {}
+    if File.file?(file_name)
+      File.readlines(file_name, chomp: true).each do |line|
+        if line
+          if line.split(' ').last == "(Definicion)"
+            question = {definition: line.split(' ')[0...-1].join(' ')}
+          end
+          if line.split(' ').last == "(Respuesta)"
+            answer = {**question, answer: line.split(' ')[0...-1].join(' ')}
+            questions_list.push(answer)
+          end
+        end
+      end
+    end
+    @questions_list = questions_list
+    return @questions_list
+  end
+
+
+
+
+  def reto5(questions = @questions_list)
+    puts 'Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!'
+
+    i=0
+    while  i < questions.length
+      puts 'Definición'
+      puts questions[i][:definition]
+      print 'Adivina: '
+      ans = gets.chomp.upcase
+      while ans != questions[i][:answer].upcase
+        puts 'Incorrecto!, Trata de nuevo'
+        print 'Adivina: '
+        ans = gets.chomp.upcase
+      end
+      puts 'Correcto!'
+      i += 1
+    end
+    puts 'Felicidades! Has ganado!'
+
+
+  end
+
+
+
+
+end
+
+reto_5 = Reto_5.new('preguntas.txt')
+reto_5.reto5
