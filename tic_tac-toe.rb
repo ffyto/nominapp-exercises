b48529681f1e6a185b3dc2913b9f52beaecc068b
mauro <cmauricio.arango@gmail.com>
2022-12-19 14:44:45 -0500

    refactoring tic tac toe and Reto5 exercises

--- tic_tac-toe.rb
+++ tic_tac-toe.rb
@@ -1,77 +1,79 @@
 class TicTacToe
-
-  def table(positions)
-    x = 0
-    for i in 1..3 do
-      for j in 1..3 do
-        print "#{positions[x]} "
-        x += 1
+  private
+    def table(positions)
+      x = 0
+      for i in 1..3 do
+        for j in 1..3 do
+          print "#{positions[x]} "
+          x += 1
+        end
+        puts ""
       end
-      puts ""
     end
-  end
 
-  def game
-
-    positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    puts "Let's start the game!"
-    puts ""
-    table(positions)
-    puts ""
-
-    i=0
-    while i < 9 do
-      if i % 2 == 0
-        print "Player X select a position: "
-        player = "X"
-      else
-        print "Player O select a position: "
-        player = "O"
+    def result(positions)
+      if positions[0] == positions[1] && positions[1] == positions[2]
+        return "Player #{positions[0]} has won!"
       end
-      position = gets.chomp.to_i
-      change = positions.find_index(position)
-      while !change do
-        print "Incorrect position or position already selected. Select a position: "
-        position = gets.chomp.to_i
-        change = positions.find_index(position)
+      if positions[3] == positions[4] && positions[4] == positions[5]
+        return "Player #{positions[3]} has won!"
       end
-      positions[change] = player
-      i += 1
+      if positions[6] == positions[7] && positions[7] == positions[8]
+        return "Player #{positions[3]} has won!"
+      end
+      if positions[0] == positions[3] && positions[3] == positions[6]
+        return "Player #{positions[0]} has won!"
+      end
+      if positions[1] == positions[4] && positions[4] == positions[7]
+        return "Player #{positions[1]} has won!"
+      end
+      if positions[2] == positions[5] && positions[5] == positions[8]
+        return "Player #{positions[0]} has won!"
+      end
+      if positions[0] == positions[4] && positions[4] == positions[8]
+        return "Player #{positions[0]} has won!"
+      end
+      if positions[2] == positions[4] && positions[4] == positions[6]
+        return "Player #{positions[2]} has won!"
+      end
+      if !positions.any? (Integer)
+        return "This match ends with a tie"
+      end
+    end
+
+  public
+    def game
+      positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      puts "Let's start the game!"
       puts ""
       table(positions)
       puts ""
-      if positions[0] == positions[1] && positions[1] == positions[2]
-        return puts "Player #{positions[0]} has won!"
-      end
-      if positions[3] == positions[4] && positions[4] == positions[5]
-        return puts "Player #{positions[3]} has won!"
-      end
-      if positions[6] == positions[7] && positions[7] == positions[8]
-        return puts "Player #{positions[3]} has won!"
-      end
-      if positions[0] == positions[3] && positions[3] == positions[6]
-        return puts "Player #{positions[0]} has won!"
-      end
-      if positions[1] == positions[4] && positions[4] == positions[7]
-        return puts "Player #{positions[1]} has won!"
-      end
-      if positions[2] == positions[5] && positions[5] == positions[8]
-        return puts "Player #{positions[0]} has won!"
-      end
-      if positions[0] == positions[4] && positions[4] == positions[8]
-        return puts "Player #{positions[0]} has won!"
-      end
-      if positions[2] == positions[4] && positions[4] == positions[6]
-        return puts "Player #{positions[2]} has won!"
-      end
-      if !positions.any? (Integer)
-        return puts "This match ends with a tie"
+      i=0
+      while i < 9 do
+        if i % 2 == 0
+          print "Player X select a position: "
+          player = "X"
+        else
+          print "Player O select a position: "
+          player = "O"
+        end
+        position = gets.chomp.to_i
+        change = positions.find_index(position)
+        while !change do
+          print "Incorrect position or position already selected. Select a position: "
+          position = gets.chomp.to_i
+          change = positions.find_index(position)
+        end
+        positions[change] = player
+        i += 1
+        puts ""
+        table(positions)
+        puts ""
+        if result(positions).instance_of?(String)
+          return puts result(positions)
+        end
       end
     end
-
-  end
-
 end
 
 new_game = TicTacToe.new
